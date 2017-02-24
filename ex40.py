78913fed8285f6678def7768b68d52d9d9e61271
Ahmed Syed <Patt3rnlogic@MacBook-Pro-8.local>
2017-02-23 23:02:40 -0800

    update

---
+++ ex40.py
@@ -0,0 +1,33 @@
+class Song(object):
+	
+	def __init__(self, lyrics):	
+		self.lyrics = lyrics
+	
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print line
+
+
+happy_bday = Song(["Happy birthday to you",
+				  "I don't want to get sued",
+				  "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around the family",
+					   "With pockets full of shells"])
+
+queen = ["Is this real life",
+				 "Or is this just fantasy",
+				 "Caught in a landslide",
+				  "escape from reality"]
+
+queen_rap = Song(queen)
+	
+	
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+queen_rap.sing_me_a_song()
+	
+
+		
